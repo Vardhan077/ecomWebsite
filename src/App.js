ab30390ebc2707d43fe89e73889469d135468e0b
Vardhan077 <vardhanmarriwada777@gmail.com>
2024-02-02 15:04:24 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import './styles/App.css';
 import Home from './components/home';
-import Products from './components/Products';
+import Products from "./components/Products";
 
 
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 const App = () =>(
-  <BrowserRouter  basename={'/ecomWebsite'} >
+  <BrowserRouter  basename='/ecomWebsite/' >
         <Routes>
           <Route exact path='/' element={<Home />}></Route>
-          <Route exact path='/products' element = {<Products/>}></Route>
+          <Route path='/products' element = {<Products/>}></Route>
         </Routes>
       </BrowserRouter>
 )
